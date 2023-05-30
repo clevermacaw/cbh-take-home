975579bbb56bd954e538c83a47a40a32d0900609
clevermacaw <cleverbluemaccaw@gmail.com>
2023-05-30 06:10:20 -0400

    Refactored

--- dpk.test.js
+++ dpk.test.js
@@ -6,6 +6,7 @@ describe('deterministicPartitionKey', () => {
     const result = deterministicPartitionKey(event)
     expect(result).toEqual('testKey')
   })
+
   it('should return a hashed partition key if the partition key does not exist', () => {
     const event = { someData: 'testData' }
     const result = deterministicPartitionKey(event)
@@ -13,6 +14,7 @@ describe('deterministicPartitionKey', () => {
       '1b844e9d9d9e43d6f2492b194a12246501a0ce15691b7579a294d8fa07b1cd41bdfc4eb9814c4f50ad0594fbecf4a17b9ac0b8d3f47ed2466c1b25d79c4508f3'
     )
   })
+
   it('should return a hashed partition key if the partition key is not a string', () => {
     const event = { partitionKey: { someData: 'testData' } }
     const result = deterministicPartitionKey(event)
@@ -20,6 +22,7 @@ describe('deterministicPartitionKey', () => {
       'd5c5fa5a5a5c5d5f5e25262c413d03f9587f788da748a148aa7cc0d181af3e7a2c3b9e9c3888c087bba7cc207ae5d63a4f4d4ad4ad4ad4ad4ad4ad4ad4ad4ad4a0'
     )
   })
+
   it('should return a trivial partition key if the candidate is too long', () => {
     const longString = 'a'.repeat(257)
     const event = { someData: longString }
@@ -28,16 +31,19 @@ describe('deterministicPartitionKey', () => {
       'c6f1b7c37b98e06b2d27f0471a9b9f81b65c12178d1c23119fde7dd30c3e3de8'
     )
   })
+
   it('should return the trivial partition key if the event is null', () => {
     const event = null
     const result = deterministicPartitionKey(event)
     expect(result).toEqual('0')
   })
+
   it('should return the trivial partition key if the event is undefined', () => {
     const event = undefined
     const result = deterministicPartitionKey(event)
     expect(result).toEqual('0')
   })
+
   it('should return the trivial partition key if the event has no properties', () => {
     const event = {}
     const result = deterministicPartitionKey(event)
--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 const { deterministicPartitionKey } = require('./dpk')
+
 console.log(deterministicPartitionKey())
